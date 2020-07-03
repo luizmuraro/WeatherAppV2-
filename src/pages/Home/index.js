c509c605bc19928421acc2f4fe9d9a75164e5df0
Muraro <luizmuraro@gmail.com>
2020-07-03 18:54:54 -0300

    Add gradient backgroundColor

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,39 +2,43 @@ import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import Header from '../../components/Header';
 
+import LinearGradient from 'react-native-linear-gradient';
+
 const Home = () => {
     return (
 
-        <View style={styles.container}>
+        <LinearGradient colors={['rgb(102,197,255)', 'rgb(21,128,253)']} style={styles.container}>
             <Header />
             <View style={styles.main}>
                 <Image style={{ width: 160, height: 160 }} source={require('../../assets/sunny/imageSunny.png')} />
                 <Text>Sunny</Text>
                 <Text>22ºC</Text>
-                <View style={{ flexDirection: 'row' }}>
+                <View style={styles.minMax}>
                     <Text>24º</Text>
                     <Text>18º</Text>
                 </View>
-                <View style={{ flexDirection: 'row' }}>
+                <View style={styles.weatherConditions}>
                     <Text>50%</Text>
                     <Text>2km/h</Text>
                     <Text>20mm</Text>
                 </View>
-                <View style={{ flexDirection: 'row' }}>
+                <Text>Divider</Text>
+
+                <View style={styles.footer}>
                     <Image style={{ width: 40, height: 40 }} source={require('../../assets/sunny/imageSunny.png')} />
                     <View>
                         <Text>Tomorrow</Text>
-                        <View style={{ flexDirection: 'row' }}>
+                        <View style={styles.tomorrowMinMax}>
                             <Text>24º</Text>
                             <Text>18º</Text>
                         </View>
                     </View>
-                    <TouchableOpacity style={{alignItems: 'center', justifyContent: 'center',}}>
+                    <TouchableOpacity style={styles.button}>
                         <Text>See the full week</Text>
                     </TouchableOpacity>
                 </View>
             </View>
-        </View>
+        </LinearGradient>
     );
 }
 
@@ -46,8 +50,23 @@ const styles = StyleSheet.create({
     },
     main: {
         alignItems: 'center'
+    },
+    minMax: {
+        flexDirection: 'row'
+    },
+    weatherConditions: {
+        flexDirection: 'row'
+    },
+    footer: {
+        flexDirection: 'row'
+    },
+    tomorrowMinMax: {
+        flexDirection: 'row'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center'
     }
-
 })
 
 export default Home;
